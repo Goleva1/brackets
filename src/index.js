cd1df65c09d43c155e6b34a5530508b7bdcd0d9d
github-Goleva1 <golevaviktoria1@gmai.com>
2024-08-15 20:44:25 +0200

    fead: solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const stack = [];
+  const openBrackets = {};
+  const closeBrackets = {};
+
+  
+  for (const [open, close] of bracketsConfig) {
+      openBrackets[open] = close;
+      closeBrackets[close] = open;
+  }
+
+  for (const char of str) {
+      
+      if (openBrackets[char] && (!stack.length || openBrackets[char] !== char || stack[stack.length - 1] !== char)) {
+          stack.push(char);
+      }
+      
+      else if (closeBrackets[char]) {
+          if (stack.length === 0 || stack[stack.length - 1] !== closeBrackets[char]) {
+              return false;
+          }
+          stack.pop();
+      }
+  }
+
+
+  return stack.length === 0;
+};
